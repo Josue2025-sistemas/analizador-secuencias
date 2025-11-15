61a169400559bdf588f8b8c1702c2ca04f71bce0
Josue2025-sistemas <ggarc8413@gmail.com>
2025-11-15 08:01:27 -0500

    parcial 2

---
+++ Parcial2.java
@@ -0,0 +1,16 @@
+public class parcial {
+    public static void main(String[] args) throws Exception {
+    int[] leerArreglo(int n)
+
+boolean esPrimo(int x)
+
+int contarPrimos(int[] a)
+
+int maximo(int[] a)
+
+int[] indicesDeMaximo(int[] a)
+
+boolean esCapicua(int[] a)
+
+    }
+}
